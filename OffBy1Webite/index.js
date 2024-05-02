2299ed98573e89e140c899894b64a8833f0cf1ce
Drakocopter <lightningstaff44@gmail.com>
2024-05-02 05:32:51 -0400

    document loaded event listener change

--- OffBy1Webite/index.js
+++ OffBy1Webite/index.js
@@ -1,17 +1,12 @@
+document.addEventListener("DOMContentLoaded", handleLoaded);
+
+function handleLoaded() {
+    submitButton.addEventListener("click", handleClick);
+}
 
 function handleClick() {
     let rawText = dateInput.value;
     let dateObject = new Date(rawText)
     let outputMessage = dateObject.toLocaleDateString();
     outputText.innerHTML = rawText + "<br><br>" + outputMessage;
-
 }
-function handleLoaded() {
-    submitButton.addEventListener("click", handleClick);
-}
-
-document.addEventListener("DOMContentLoaded", handleLoaded);
-
-
-
-
