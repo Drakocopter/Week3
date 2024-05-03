7dc9bf4395678c9cb8f0344169c4c020d8bec697
Drakocopter <lightningstaff44@gmail.com>
2024-05-03 10:56:40 -0400

    created vacation page with html, css and javascript files

---
+++ Vactation/scripts/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    navigationBar.innerHTML =`
+    <a href="./index.html">Home</a>
+    <a href="./activities.html">Acitivities</a>
+    <a href="./dining.html">Dining</a>
+    <a href="./overnight.html">Overnight</a>
+    <a href="./about.html">About</a>`
+
+}); //END LOADED
